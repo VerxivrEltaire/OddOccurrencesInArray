807a88ae86b95401f46154b451866d8bf28f6fbf
VerxivrEltaire <kenneth.iheme@gmail.com>
2022-03-31 20:06:08 +0100

    Project Commit

---
+++ OddOccurrencesInArray/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace OddOccurrencesInArray {
+    class Solution {
+        public int solution(int[] A) {
+            return A.Aggregate((x, y) => x ^ y);
+        }
+    }
+
+    class MainClass {
+        public static void Main(string[] args) {
+            var sol = new Solution();
+            var values = new int[] { 9, 3, 9, 3, 9, 7, 7, 7, 9 };
+            Console.WriteLine(sol.solution(values));
+        }
+    }
+}
